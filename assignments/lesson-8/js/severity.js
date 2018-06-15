6f0260e2c480bce7a740bf38bccbafa21347b5f8
Derek Munson <mun17009@byui.edu>
2018-06-15 02:04:07 -0700

    Storm Center Initial Push
    First push of new Sotrm Center form page, so that it can be tested on a real mobile device.

---
+++ assignments/lesson-8/js/severity.js
@@ -0,0 +1,7 @@
+function printValue(sliderID, textbox) {
+        var x = document.getElementById(textbox);
+        var y = document.getElementById(sliderID);
+        x.value = y.value;
+    }
+
+window.onload = function() { printValue('rating', 'severityValue') };
