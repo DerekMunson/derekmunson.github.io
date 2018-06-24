f21f350263256a843b93c187b35985f1f648999f
Derek Munson <mun17009@byui.edu>
2018-06-23 17:58:39 -0700

    More assignment updates
    Added some code to taje the weather description and match it up with one of my existing SVG icons; made some other CSS fixes, as well.

--- assignments/lesson-9/js/weather.js
+++ assignments/lesson-9/js/weather.js
@@ -6,7 +6,7 @@ weatherRequest.send();
 
 weatherRequest.onload = function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
-    // console.log(weatherRequest);
+    console.log(weatherRequest);
     
     document.getElementById('summary').innerHTML = weatherData.weather[0].description;
     document.getElementById('mainTemp').innerHTML = weatherData.main.temp.toFixed(0);
@@ -15,13 +15,59 @@ weatherRequest.onload = function () {
     document.getElementById('humidity').innerHTML = weatherData.main.humidity;
     document.getElementById('windSpeed').innerHTML = weatherData.wind.speed.toFixed(0);
     document.getElementById('windDirection').innerHTML = weatherData.wind.deg;
+  
+    if (weatherData.weather[0].description === "scattered clouds") {
+        let imagesrc = 'images/cloudy.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    } else if (weatherData.weather[0].description === "broken clouds") {
+        let imagesrc = 'images/cloudy.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    } else if (weatherData.weather[0].description === "few clouds") {
+        let imagesrc = 'images/partly-cloudy.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    } else if (weatherData.weather[0].description === "clear sky") {
+        let imagesrc = 'images/sunny.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    } else if (weatherData.weather[0].description === "thunderstorm") {
+        let imagesrc = 'images/thunderstorms.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    } else if (weatherData.weather[0].description === "rain") {
+        let imagesrc = 'images/rainy.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    } else if (weatherData.weather[0].description === "light rain") {
+        let imagesrc = 'images/rainy.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    } else if (weatherData.weather[0].description === "moderate rain") {
+        let imagesrc = 'images/rainy.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    } else if (weatherData.weather[0].description === "shower rain") {
+        let imagesrc = 'images/rainy.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    } else if (weatherData.weather[0].description === "snow") {
+        let imagesrc = 'images/snow.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    } else if (weatherData.weather[0].description === "light snow") {
+        let imagesrc = 'images/snow.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    } else if (weatherData.weather[0].description === "heavy snow") {
+        let imagesrc = 'images/snow.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    } else if (weatherData.weather[0].description === "shower snow") {
+        let imagesrc = 'images/snow.svg';
+        document.getElementById('weatherimage').src = imagesrc;
+    }
     
-    let imagesrc = 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
-    document.getElementById('weatherimage').src = imagesrc;
+    
+//    let imagesrc = 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
+//    document.getElementById('weatherimage').src = imagesrc;
 }
 
 // function weatherIcon() {
-//    if (weatherData.weather[0].main == "Clouds") {
-//        console.log("Cloudy Icon");
+//    if (weatherData.weather[0].main === "Clouds") {
+//        let imagesrc = 'images/icon-cloudy.svg';
+//        document.getElementById('weatherimage').src = imagesrc;
+//    } else if (weatherData.weather[0].main === "Sunny") {
+//        let imagesrc = 'images/icon-sunny.svg';
+//        document.getElementById('weatherimage').src = imagesrc;
 //    }
 // }
